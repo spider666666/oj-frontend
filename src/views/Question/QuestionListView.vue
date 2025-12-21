f5f7296d93cb04313407b049bc09c7ecbd46a57f
菜鸟 <3572677098@qq.com>
2025-12-21 21:15:10 +0800

    fixed

--- src/views/Question/QuestionListView.vue
+++ src/views/Question/QuestionListView.vue
@@ -1,23 +1,24 @@
 <template>
-    <a-table 
-    :data="data" 
-    :pagination="pagination"
-    @page-change="handleTableChange" style="margin-top: 30px">
+    <a-table :data="data?.records" :pagination="pagination" @page-change="handleTableChange" style="margin-top: 30px">
         <template #columns>
             <a-table-column title="id" data-index="id"></a-table-column>
             <a-table-column title="题目" data-index="title"></a-table-column>
             <a-table-column title="内容" data-index="content" tooltip></a-table-column>
             <a-table-column title="标签" data-index="tags"></a-table-column>
-            <!-- <a-table-column title="判题配置" data-index="judgeConfig"></a-table-column> -->
+            <a-table-column title="判题配置" data-index="judgeConfig" tooltip>
+                <template #cell="{ record }">
+                    {{ JSON.stringify(record.judgeConfig)}}
+                </template>
+            </a-table-column>
             <a-table-column title="通过数量" data-index="acceptedNum"></a-table-column>
             <a-table-column title="提交数量" data-index="submittedNum"></a-table-column>
-            <!-- <a-table-column title="答案" data-index="answer"></a-table-column> -->
+            <a-table-column title="答案" data-index="answer"></a-table-column>
             <a-table-column title="操作栏">
                 <template #cell="{ record }">
                     <div style="display: flex; gap: 8px;">
-                    <a-button type="primary" @click="handleEditor(record)">编辑</a-button>
-                    <a-button type="primary" @click="handleView(record)">查看</a-button>
-                    <a-button status="danger" @click="handleDelete(record)">删除</a-button>
+                        <a-button type="primary" @click="handleEditor(record)">编辑</a-button>
+                        <a-button type="primary" @click="handleView(record)">查看</a-button>
+                        <a-button status="danger" @click="handleDelete(record)">删除</a-button>
                     </div>
 
                 </template>
@@ -41,14 +42,14 @@ const data = ref<Page_QuestionVO_>()
 const pagination = reactive({
     showTotal: true,
     current: 1,
-    pageSize: 1,
+    pageSize: 10,
     total: 0
 })
 
 //创建查询参数
 const queryParams = reactive<QuestionQueryRequest>({
-    current :pagination.current,
-    pageSize : pagination.pageSize
+    current: pagination.current,
+    pageSize: pagination.pageSize
 })
 
 // 或者使用下面的方法使得其变为响应式引用
@@ -67,20 +68,24 @@ watch(pagination, (newPagination) => {
 // todo 缺少权限的校验
 const loadData = async () => {
     const res = await QuestionControllerService.listQuestionVoByPageUsingPost(queryParams);
-    if(res.code === 0){
+    console.log("查询数据为：", res)
+    if (res.code === 0) {
         Message.success("查询成功")
-        data.value = res.data.records
+        data.value = res.data
+        console.log(data
+            .value
+        )
         pagination.total = res.data.total
     }
-    else{
+    else {
         Message.error("请检查网络设置")
     }
 }
 
-const handleTableChange = (page:number) =>{
-  pagination.current = page
+const handleTableChange = (page: number) => {
+    pagination.current = page
 }
-onMounted(()=>{
+onMounted(() => {
     loadData();
 })
 
@@ -88,7 +93,7 @@ const router = useRouter()
 //书写操作栏方法
 const handleEditor = (record: any) => {
     //跳转到编辑页面
-    router.push({ 
+    router.push({
         path: "/add/question",
         query: { id: record.id }
     });
