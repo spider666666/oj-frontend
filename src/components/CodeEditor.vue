cfc90476b95e866ddf36d91f655f8080333120d8
菜鸟 <3572677098@qq.com>
2025-12-22 21:42:48 +0800

    完成题目提交页面的开发

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -3,30 +3,53 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import * as monaco from 'monaco-editor';
-const value = ref("hello,world");
+import { QuestionSubmitAddRequest } from 'generated';
+const value = ref("");
 const coderef = ref();
+
+//定义事件发送
+const emit = defineEmits<{
+    (e: 'change', value: string): void;
+}>();
+
+//当code发生变化的时候发送事件
+watch(value, (newValue) => {
+    console.log("新的值为：", newValue);
+    emit('change', newValue);
+});
+
+//监控传入的参数变化
+const props = defineProps<QuestionSubmitAddRequest>();
+console.log("传入的参数为：", props);
 
 onMounted(() => {
     //挂载节点(防止元素没有加载进来，导致找不到元素报错)
-    if(coderef.value == null){
-        return 
+    if (coderef.value == null) {
+        return
     }
-    else{
+    else {
         const myEditor = monaco.editor.create(coderef.value, {
-            value:value.value,
-            language: "java",
-            automaticLayout: true,
-});
+            value: value.value,
+            language: props.language,
+            automaticLayout: true
+        });
+        // 监听 Monaco Editor 内容变化的内置事件（这是一个坑，这里存在很大的问题没有发现），注意事件的监听一定要是正确的对象
+        myEditor.onDidChangeModelContent(() => {
+            if (myEditor.getValue()) {
+                // 将编辑器内容同步到响应式变量 value
+                const newValue = myEditor.getValue();
+                value.value = newValue;
+            }
+        });
     }
 });
 
 </script>
 <style coped>
-
 #code {
-    margin-top:10px;
+    margin-top: 10px;
     width: 100%;
     height: 200px;
 }
--- src/views/Question/DoQuestionView.vue
+++ src/views/Question/DoQuestionView.vue
@@ -16,8 +16,12 @@
                 </div>
             </a-col>
             <a-col :span="12">
-                <div>这里存放的是编写代码的地方</div>
-                <code-editor />
+                <a-select :style="{ width: '320px' }" v-model="form.language" placeholder="请选择编程语言">
+                    <a-option>java</a-option>
+                    <a-option>cpp</a-option>
+                    <a-option>html</a-option>
+                </a-select>
+                <code-editor :language="form.language" @change="HandleChange" />
 
                 <a-button type="primary" @click="doSubmitQuestion">提交题目</a-button>
             </a-col>
@@ -27,12 +31,13 @@
 
 <script setup lang="ts">
 import { Message } from '@arco-design/web-vue';
-import { QuestionControllerService, QuestionVO } from "../../../generated";
-import { onMounted, ref } from 'vue';
+import { QuestionSubmitControllerService, QuestionControllerService, QuestionVO, QuestionSubmitAddRequest } from "../../../generated";
+import { onMounted, ref, reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import CodeEditor from '@/components/CodeEditor.vue';
+import { languages } from 'monaco-editor/esm/metadata';
 
-
+/*-----------------------传参相关的内容----------------*/
 
 //根据用户根据路由id传递过来的参数进行数据的查询（一定要做一个props属性的开启）
 //小细节：如果传入的数据类型不匹配，会默认使用默认值，所以要搞清楚这个函数的本质原理
@@ -42,7 +47,9 @@ const props = withDefaults(defineProps<{
     id: ""
 })
 
+
 const data = ref<QuestionVO>();
+
 // todo 缺少权限的校验
 const loadData = async () => {
     console.log("题目id为：", props.id)
@@ -56,11 +63,25 @@ const loadData = async () => {
         Message.error("请检查网络设置")
     }
 }
+/*----------------定义表单相关的接口和表单数据--------------- */
 
+//定义表单，同时进行初始化
+const form = ref<QuestionSubmitAddRequest>({
+    code: "",
+    language: "",
+    questionId: props.id
+});
 
-const doSubmitQuestion = () => {
-    console.log("提交题目")
-    Message.success("题目提交成功！")
+const doSubmitQuestion = async () => {
+    console.log("提交题目", form.value)
+    const res = await QuestionSubmitControllerService.doSubmitUsingPost(form.value);
+    if (res.code === 0) {
+        Message.success("题目提交成功！")
+    }
+    else {
+        Message.error("题目提交失败！" + res.message)
+    }
+
 
 }
 
@@ -68,6 +89,9 @@ onMounted(() => {
     loadData();
 })
 
-//这里才提交之后，可能跳转到首页页面，显示当前用户提交题目的管理页面
-const router = useRouter()
+const HandleChange = (newCode: string) => {
+    form.value.code = newCode;
+    console.log("父组件获取到的表单数据为：", form.value.code)
+
+}
 </script>
